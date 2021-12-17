5917d75a248d19062b5fe91efbc6e3ce93aad303
NeptunePurpleHeart <anna.bekker2401@gmail.com>
2021-12-17 04:21:55 +0200

    Completed file loader

--- src/components/TableReader.ts
+++ src/components/TableReader.ts
@@ -1,2 +1,42 @@
 export default class TableReader {
+    private element: Element | null = document.querySelector('.reader');
+    private isShown: boolean = false;
+    private reader: FileReader = new FileReader();
+    public isSubmitted: boolean = false;
+
+    constructor() {
+        this.render();
+    }
+
+    private submitHandler(event: Event | undefined) {
+        event?.preventDefault();
+        const target = (<HTMLFormElement>event?.target);
+        const file: any = (target.children[1] as unknown as FileList);
+        this.reader.readAsText(file.files[0]);
+        this.isSubmitted = true;
+    }
+
+    public getRawText() {
+        return new Promise(((resolve) => {
+            this.reader.onload = (e) => {
+                resolve(e.target?.result);
+            };
+        }));
+    }
+
+    public show() {
+        this.element?.classList.toggle('visible');
+        this.isShown = !this.isShown;
+    }
+
+    public render() {
+        this.element!.innerHTML = '';
+        this.element?.insertAdjacentHTML('beforeend',
+            `<form class="fileInput">
+                      <label for="file">Завантажте файл</label>
+                      <input type="file" name="file" id="file" accept=".txt" required>
+                      <input type="submit" value="Завантажити таблицю">
+            </form>`);
+        document.querySelector('.fileInput')?.addEventListener('submit', () => this.submitHandler(event));
+    }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import Table from "./components/Table";
-import TableReader from "./components/TableReader"
+import TableReader from "./components/TableReader";
+import './style.css';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const tableReader = new TableReader();
+    tableReader.show();
+    const text = await tableReader.getRawText();
+    console.log(text);
     const table: Table = new Table();
     table.render('#app');
 });
